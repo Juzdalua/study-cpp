deae59ea47867d7c585121c9a716f5c529c503f4
juzdalua <bearwcw@naver.com>
2024-07-20 19:09:11 +0900

    First Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+#define ABC 1;
+int main(){
+    cout << "Hello, World!" << endl;
+    cout << "Hello, World!" << endl;
+    cout << "Hello, World!" << endl;
+
+    unsigned char a = 1;
+ 
+
+    cout << "A: " << a << endl;
+    cout << "Hello, World!" << endl;
+    
+    int abc = ABC;
+    cout << abc << flush;
+
+    return 0;
+}

e51f5d1ffda18ac07d24091cfa1ff32f4c7183c6
juzdalua <bearwcw@naver.com>
2024-07-30 17:14:11 +0900

    Add condition_varibale

--- main.cpp
+++ main.cpp
@@ -1,72 +1,46 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
-#include <atomic>
 
-class SpinLock {
-public:
-	void lock() {
-		bool expected = false; // before
-		bool desired = true; // after
+mutex m;
+queue<int> q;
+condition_variable cv;
 
-		while (_locked.compare_exchange_strong(expected, desired) == false) {
-			expected = false;
+void Producer() {
+	while (true) {
+		{
+			/*
+				1. Lock을 잡고
+				2. 공유 변수 값을 수정
+				3. Lock을 풀고
+				4. 조건변수를 통해 다른 스레드에게 통지
+			*/
+			unique_lock<mutex> lock(m);
+			q.push(100);
 		}
+		cv.notify_one(); // wait중인 스레드 1개를 깨운다.
 	}
-	void unlock() {
-		//_locked = false;
-		_locked.store(false);
-	}
-private:
-	atomic<bool> _locked = false;
-};
 
-class SleepLock {
-public:
-	void lock() {
-		bool expected = false; // before
-		bool desired = true; // after
-
-		while (_locked.compare_exchange_strong(expected, desired) == false) {
-			expected = false;
-
-			this_thread::sleep_for(0ms);
-		}
-	}
-	void unlock() {
-		//_locked = false;
-		_locked.store(false);
-	}
-private:
-	atomic<bool> _locked = false;
-};
-
-
-SpinLock spinLock;
-int sum = 0;
-
-void Add() {
-	for (int i = 0; i < 1'000'000; i++) {
-		lock_guard<SpinLock> guard(spinLock);
-		sum++;
-	}
 }
 
-void Sub() {
-	for (int i = 0; i < 1'000'000; i++) {
-		lock_guard<SpinLock> guard(spinLock);
-		sum--;
+void Consumer() {
+	while (true) {
+		unique_lock<mutex> lock(m);
+		cv.wait(lock, []() {return q.empty() == false;});
+
+		int data = q.front();
+		q.pop();
+		cout << data << endl;
 	}
 }
-
 
 int main()
 {
-	thread t1(Add);
-	thread t2(Sub);
+	thread t1(Producer);
+	thread t2(Consumer);
+
 	t1.join();
 	t2.join();
-	cout << "SpinLock: " << sum << endl;
 
 	return 0;
 }
